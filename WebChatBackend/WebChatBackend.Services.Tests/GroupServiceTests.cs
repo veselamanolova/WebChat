0819b3d31410c72d4da8468d800a9a26fa3c1a49
veselamanolova <veselamanolov@abv.bg>
2019-09-27 09:28:23 +0300

    GroupServiceTests

--- WebChatBackend/WebChatBackend.Services.Tests/GroupServiceTests.cs
+++ WebChatBackend/WebChatBackend.Services.Tests/GroupServiceTests.cs
@@ -19,85 +19,201 @@ namespace WebChatBackend.Services.Tests
         [TestMethod]
         public async Task CreateNewGroupAsyncShould_CreatePrivateChatIf2UserIdsAreProvided()
         {
-            var options = new DbContextOptionsBuilder()
-               .UseInMemoryDatabase(databaseName: "CreateNewGroupAsyncShould_CreatePrivateChatIf2UserIdsAreProvided")
-               .Options;
-
-            using (var actAndAssertContext =
-                new WebChatContext(TestUtils.GetOptions(nameof(CreateNewGroupAsyncShould_CreatePrivateChatIf2UserIdsAreProvided))))
+            const string databaseName = nameof(CreateNewGroupAsyncShould_CreatePrivateChatIf2UserIdsAreProvided);
+            using (var arrangeContext = new WebChatContext(TestUtils.GetOptions(databaseName)))
             {
-                GroupService sut = new GroupService(actAndAssertContext);
+                await arrangeContext.Users.AddAsync(new User { Id = "1", UserName = "User1" });
+                await arrangeContext.Users.AddAsync(new User { Id = "2", UserName = "User2" });
+                await arrangeContext.SaveChangesAsync();
+            }
+
+            using (var actContext = new WebChatContext(TestUtils.GetOptions(databaseName)))
+            {
+                GroupService sut = new GroupService(actContext);
                 CreateGroupRequest groupRequest = new CreateGroupRequest()
                 {
                     Name = null,
                     UserIds = new List<string>(new string[] { "1", "2" })
                 };
-               
-                  var user1 = new User()
-                {
-                    Id = "1",
-                    UserName = "User1"
-                };
-                var user2 = new User()
-                {
-                    Id = "2",
-                    UserName = "User2"
-                };
-
-                await actAndAssertContext.Users.AddAsync(user1);
-                await actAndAssertContext.Users.AddAsync(user2);                
-
                 var result = await sut.CreateNewGroupAsync(groupRequest, "1");
-               
+
                 Assert.AreEqual(true, result.IsPrivateChat);
-                Assert.IsTrue(DateTime.UtcNow- result.LastActivityDate < TimeSpan.FromSeconds(2));
-            };
+                Assert.IsTrue(DateTime.UtcNow - result.LastActivityDate < TimeSpan.FromSeconds(2));
+            }
         }
 
         [TestMethod]
         public async Task CreateNewGroupAsyncShould_CreateGroupChatIf3UserIdsAreProvided()
         {
-            var options = new DbContextOptionsBuilder()
-               .UseInMemoryDatabase(databaseName: "CreateNewGroupAsyncShould_CreateGroupChatIf3UserIdsAreProvided")
-               .Options;
-
-            using (var actAndAssertContext =
-                new WebChatContext(TestUtils.GetOptions(nameof(CreateNewGroupAsyncShould_CreateGroupChatIf3UserIdsAreProvided))))
+            const string databaseName = nameof(CreateNewGroupAsyncShould_CreateGroupChatIf3UserIdsAreProvided);
+            using (var arrangeContext = new WebChatContext(TestUtils.GetOptions(databaseName)))
             {
-                GroupService sut = new GroupService(actAndAssertContext);
+                await arrangeContext.Users.AddAsync(new User { Id = "1", UserName = "User1" });
+                await arrangeContext.Users.AddAsync(new User { Id = "2", UserName = "User2" });
+                await arrangeContext.Users.AddAsync(new User { Id = "3", UserName = "User3" });
+                await arrangeContext.SaveChangesAsync();
+            }
+
+            using (var actContext = new WebChatContext(TestUtils.GetOptions(databaseName)))
+            {
+                GroupService sut = new GroupService(actContext);
                 CreateGroupRequest groupRequest = new CreateGroupRequest()
                 {
                     Name = "test",
-                    UserIds = new List<string>(new string[] { "1", "2" , "3"})
+                    UserIds = { "1", "2", "3" }
                 };
-
-                var user1 = new User()
-                {
-                    Id = "1",
-                    UserName = "User1"
-                };              
-                var user2 = new User()
-                {
-                    Id = "2",
-                    UserName = "User2"
-                };
-                var user3 = new User()
-                {
-                    Id = "3",
-                    UserName = "User3"
-                };
-
-                await actAndAssertContext.Users.AddAsync(user1);
-                await actAndAssertContext.Users.AddAsync(user2);
-                await actAndAssertContext.Users.AddAsync(user3);
-
                 var result = await sut.CreateNewGroupAsync(groupRequest, "1");
 
                 Assert.AreEqual("test", result.Name);
                 Assert.AreEqual(false, result.IsPrivateChat);
                 Assert.IsTrue(DateTime.UtcNow - result.LastActivityDate < TimeSpan.FromSeconds(2));
             };
-        }    
+        }
+
+        [TestMethod]
+        public async Task CreateNewGroupAsyncShould_ReturntheSameChatIfItAlreadyExists()
+        {
+            const string databaseName = nameof(CreateNewGroupAsyncShould_ReturntheSameChatIfItAlreadyExists); 
+            using (var arrangeContext = new WebChatContext(TestUtils.GetOptions(databaseName)))
+            {
+                var user1 = new User() { Id = "1", UserName = "User1" };
+                var user2 = new User() { Id = "2", UserName = "User2" };
+
+                await arrangeContext.Users.AddAsync(user1);
+                await arrangeContext.Users.AddAsync(user2);
+
+                var UserGroup1 = new UserGroup(){UserId = "1",GroupId = 1, User = user1};
+                var UserGroup2 = new UserGroup() { UserId = "2", GroupId = 1, User = user2 };
+               
+                var Group1UserGroups = new List<UserGroup>();
+                Group1UserGroups.Add(UserGroup1);
+                Group1UserGroups.Add(UserGroup2);
+              
+                var oldGroup = new Group()
+                {
+                    Id = 1,
+                    IsPrivateChat = true,
+                    LastActivityDate = new DateTime(2019, 5, 5),
+                    UserGroups = Group1UserGroups
+                };
+                await arrangeContext.Groups.AddAsync(oldGroup);
+                arrangeContext.SaveChanges();
+            }
+
+
+            using (var actContext  =
+               new WebChatContext(TestUtils.GetOptions(databaseName)))
+            {
+                GroupService sut = new GroupService(actContext);
+
+                var groupRequest = new CreateGroupRequest(){ Name = null, UserIds = { "1", "2" }};
+                var result = await sut.CreateNewGroupAsync(groupRequest, "1");
+
+                Assert.AreEqual(1, result.Id);
+                Assert.AreEqual(true, result.IsPrivateChat);
+                Assert.AreEqual(new DateTime(2019, 5, 5),result.LastActivityDate);
+            };
+        }
+
+
+        [TestMethod]
+        public async Task GetUserGroupsAsyncShould_ReturnUserGroups()
+        {
+            const string databaseName = nameof(GetUserGroupsAsyncShould_ReturnUserGroups); 
+
+            using (var arrangeContext =
+                new WebChatContext(TestUtils.GetOptions(databaseName)))
+            {               
+                CreateGroupRequest groupRequest = new CreateGroupRequest()
+                {
+                    Name = null,
+                    UserIds = { "1", "2" }
+                };
+
+                var user1 = new User() { Id = "1", UserName = "User1" };
+                var user2 = new User() { Id = "2", UserName = "User2" };              
+
+                await arrangeContext.Users.AddAsync(user1);
+                await arrangeContext.Users.AddAsync(user2);
+
+                var UserGroup1 = new UserGroup() { UserId = "1", GroupId = 1, User = user1 };
+                var UserGroup2 = new UserGroup() { UserId = "2", GroupId = 1, User = user2 };
+
+                var Group1UserGroups = new List<UserGroup>{ UserGroup1, UserGroup2 };
+
+                await arrangeContext.UserGroups.AddAsync(UserGroup1);
+                await arrangeContext.UserGroups.AddAsync(UserGroup2);
+
+                var Group = new Group()
+                {
+                    Id = 1,
+                    IsPrivateChat = true,
+                    LastActivityDate = new DateTime(2019, 5, 5),
+                    UserGroups = Group1UserGroups
+                };
+                await arrangeContext.Groups.AddAsync(Group);
+                arrangeContext.SaveChanges();
+            }
+
+            using (var actContext =
+                new WebChatContext(TestUtils.GetOptions(databaseName)))
+            {
+                GroupService sut = new GroupService(actContext);
+              
+                var result = await sut.GetUserGroupsAsync("1");
+                Assert.AreEqual(1, result.Count);
+                Assert.AreEqual(1, result[0].Id);                
+            };
+        }
+
+
+        [TestMethod]
+        public async Task GetUserGroupsAsyncShould_ReturnEmptyUserGroupsListIfNoUserGroupsExist()
+        {
+            const string databaseName = nameof(GetUserGroupsAsyncShould_ReturnEmptyUserGroupsListIfNoUserGroupsExist);
+
+            using (var arrangeContext =
+                new WebChatContext(TestUtils.GetOptions(databaseName)))
+            {
+                CreateGroupRequest groupRequest = new CreateGroupRequest()
+                {
+                    Name = null,
+                    UserIds = { "3", "2" }
+                };
+
+                var user2 = new User() { Id = "2", UserName = "User2" };
+                var user3 = new User() { Id = "3", UserName = "User3" };
+
+                await arrangeContext.Users.AddAsync(user2);
+                await arrangeContext.Users.AddAsync(user3);
+               
+                var UserGroup2 = new UserGroup() { UserId = "2", GroupId = 1, User = user2 };
+                var UserGroup1 = new UserGroup() { UserId = "3", GroupId = 1, User = user3 };
+
+                var Group1UserGroups = new List<UserGroup> { UserGroup1, UserGroup2 };
+
+                await arrangeContext.UserGroups.AddAsync(UserGroup1);
+                await arrangeContext.UserGroups.AddAsync(UserGroup2);
+
+                var Group = new Group()
+                {
+                    Id = 1,
+                    IsPrivateChat = true,
+                    LastActivityDate = new DateTime(2019, 5, 5),
+                    UserGroups = Group1UserGroups
+                };
+                await arrangeContext.Groups.AddAsync(Group);
+                arrangeContext.SaveChanges();
+            }
+
+            using (var actContext =
+                new WebChatContext(TestUtils.GetOptions(databaseName)))
+            {
+                GroupService sut = new GroupService(actContext);
+                var result = await sut.GetUserGroupsAsync("1");
+                Assert.AreEqual(0, result.Count);                
+            };
+        }
 
 
     }
